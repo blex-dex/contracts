e025a21b13546f2f75c21663a83e35cee5d072e1
Tim <aitimate@dalveytech.com>
2023-09-11 15:55:55 +0800

    fix: BLX-9
    fix: BLX-9
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    fix: BLX-9 with doc.
    
    rm unit test case

--- contracts/oracle/ChainPriceFeed.sol
+++ contracts/oracle/ChainPriceFeed.sol
@@ -6,8 +6,8 @@ import "../ac/Ac.sol";
 
 contract ChainPriceFeed is Ac {
     uint256 public constant PRICE_PRECISION = 10 ** 30;
-    uint256 public sampleSpace = 3;
 
+    uint256 public sampleSpace = 3;
     mapping(address => address) public priceFeeds;
     mapping(address => uint256) public priceDecimals;
 
@@ -27,64 +27,39 @@ contract ChainPriceFeed is Ac {
         priceDecimals[_token] = _decimal;
     }
 
-    function getLatestPrice(address _token) public view returns (uint256) {
-        address _feed = priceFeeds[_token];
-        require(_feed != address(0), "PriceFeed: invalid price feed");
+    function getLatestPrice(address token) public view returns (uint256) {
+        address feed = priceFeeds[token];
+        require(feed != address(0), "PriceFeed: invalid price feed");
 
-        IPriceFeed _priceFeed = IPriceFeed(_feed);
+        (,int256 price,,,) = IPriceFeed(feed).latestRoundData();
+        require(price > 0, "PriceFeed: invalid price");
 
-        int256 _price = _priceFeed.latestAnswer();
-        require(_price > 0, "PriceFeed: invalid price");
-
-        return uint256(_price);
+        return uint256(price);
+        // return _getPrice(token, false, 1);
     }
 
-    function getPrice(
-        address _token,
-        bool _maximise
-    ) public view returns (uint256) {
-        address _feed = priceFeeds[_token];
-        require(_feed != address(0), "PriceFeed: invalid price feed");
+    function getPrice(address token, bool maximise) public view returns (uint256) {
+        return _getPrice(token, maximise, uint80(sampleSpace));
+    }
 
-        IPriceFeed _priceFeed = IPriceFeed(_feed);
+    function _getPrice(address token, bool maximise, uint80 rounds_) internal view returns (uint256 finalPrice) {
+        address feed = priceFeeds[token];
+        require(feed != address(0), "PriceFeed: invalid price feed");
 
-        uint256 _price = 0;
-        uint80 _id = _priceFeed.latestRound();
-
-        for (uint80 i = 0; i < sampleSpace; i++) {
-            if (_id <= i) {
-                break;
-            }
-            uint256 p;
-
-            if (i == 0) {
-                int256 _p = _priceFeed.latestAnswer();
-                require(_p > 0, "PriceFeed: invalid price");
-                p = uint256(_p);
+        IPriceFeed priceFeed = IPriceFeed(feed);
+        (uint80 roundId, int256 price,,,) = priceFeed.latestRoundData();
+        for (uint80 end = roundId-- - rounds_; roundId > end; roundId--) {
+            (, int256 _price, , ,) = priceFeed.getRoundData(roundId);
+            if (maximise) {
+                if (_price > price) price = _price;
             } else {
-                (, int256 _p, , , ) = _priceFeed.getRoundData(_id - i);
-                require(_p > 0, "PriceFeed: invalid price");
-                p = uint256(_p);
-            }
-
-            if (_price == 0) {
-                _price = p;
-                continue;
-            }
-
-            if (_maximise && p > _price) {
-                _price = p;
-                continue;
-            }
-
-            if (!_maximise && p < _price) {
-                _price = p;
+                if (_price < price) price = _price;
             }
         }
+        uint8 decimals = priceFeed.decimals();
+        finalPrice = (uint256(price) * PRICE_PRECISION) / (10 ** decimals);
 
-        require(_price > 0, "PriceFeed: could not fetch price");
-
-        uint256 _decimals = priceDecimals[_token];
-        return (_price * PRICE_PRECISION) / (10 ** _decimals);
+        require(finalPrice > 0, "PriceFeed: could not fetch price");
+        return finalPrice;
     }
 }
